40ddbf15cf9955d80d7fd1cac56b3e298298d349
deeba <adibah@hotmail.sg>
2023-05-10 23:26:44 +0800

    init frontend with homepage completed

---
+++ frontend/src/home/navbar/Navbar.js
@@ -0,0 +1,71 @@
+import React, {useState} from 'react';
+import {RiMenu3Line, RiCloseLine} from 'react-icons/ri';
+import './navbar.css';
+import PetsIcon from '@mui/icons-material/Pets';
+
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+  return (
+    <div className='pml__navbar'>
+      <div className='pml__navbar-links'>
+        <div className='pml__navbar-title'>
+          <PetsIcon /> MeowLand
+        </div>
+        <div className='pml__navbar-links_container'>
+          <p>
+            <a href='/'>Home</a>
+          </p>
+          <p>
+            <a href='/forum'>Forum</a>
+          </p>
+          <p>
+            <a href='/breed'>Breeds</a>
+          </p>
+        </div>
+      </div>
+      <div className='pml__navbar-sign'>
+        <button type='button'>
+          <a href='/sign'>Login/Register</a>
+        </button>
+      </div>
+      <div className='pml__navbar-menu'>
+        {toggleMenu ? (
+          <RiCloseLine
+            color='#fff'
+            size={27}
+            onClick={() => setToggleMenu(false)}
+          />
+        ) : (
+          <RiMenu3Line
+            color='#fff'
+            size={27}
+            onClick={() => setToggleMenu(true)}
+          />
+        )}
+        {toggleMenu && (
+          <div className='pml__navbar-menu_container scale-up-center'>
+            <div className='pml__navbar-menu_container-links'>
+              <p>
+                <a href='/'>Home</a>
+              </p>
+              <p>
+                <a href='/forum'>Forum</a>
+              </p>
+              <p>
+                <a href='/breed'>Breeds</a>
+              </p>
+            </div>
+            <div className='pml__navbar-menu_container-links-sign'>
+              <button type='button'>
+                <a href='/sign'>Login/Register</a>
+              </button>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
